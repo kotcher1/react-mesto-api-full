3eab4b6809d1bf2f9a70fd905abbc8a6aafee095
Anna Shulgina <anna.shulgina@digitalizm.com>
2021-12-18 18:28:09 +0300

    feat: CORS settings

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,27 @@ const NotFoundError = require('./errors/not-found-error');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
+const allowedCors = [
+  'localhost:3000'
+];
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE"; 
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+      res.header('Access-Control-Allow-Headers', requestHeaders);
+      return res.end();
+  }
+  next();
+}); 
+
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
