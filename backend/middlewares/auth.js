7504383f71609df81c418cadbe36e896f3e96a87
kotcher1 <annashkt@gmail.com>
2021-12-19 14:06:50 +0300

    fix

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,14 +4,17 @@ const AuthError = require('../errors/auth-error');
 module.exports = (req, res, next) => {
   const { NODE_ENV, JWT_SECRET } = process.env;
   const authorization = req.headers;
-  if (!authorization) {
-    return next(new AuthError('Необходима авторизация'));
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new AuthError('Ошибка авторизации');
   }
+
+  const token = authorization.replace('Bearer ', '');
   let payload;
+
   try {
-    payload = jwt.verify(authorization, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
   } catch (err) {
-    return next(new AuthError('Необходима авторизация'));
+    next(new AuthError('Ошибка авторизации'));
   }
 
   req.user = payload;
